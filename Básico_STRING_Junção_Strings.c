a018e0a1877fa81446cbb9b39e1418f80d9e05f4
kennyow <114197131+kennyow@users.noreply.github.com>
2024-01-15 08:42:15 -0300

    Update Básico_STRING_Junção_Strings.c

--- Básico_STRING_Junção_Strings.c
+++ Básico_STRING_Junção_Strings.c
@@ -10,12 +10,19 @@ printf("******* Biblioteca *********\n");
 
 int main() {
 
+
 char nome1[30], nome2[30];
 printf("/n");
 //Header function
 escreveCabecalho();
 
+printf("Inserisci un nome: \n");
+scanf("%s", nome1);
 
+printf("Inserisci un altro nome: \n");
+scanf("%s", nome2);
+
+printf("%s", nome1);
 //printf("Os nomes emendados fica %s", strcat(nome1, nome2));
 }
 
